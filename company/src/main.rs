c735ae9ea3d7433d8c40a60883089543199d3201
Trevor Amburgey <tamburgey@mitre.org>
2021-02-15 11:23:39 -0500

    template for 8-3 exercise

---
+++ company/src/main.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+fn main() {
+    // department -> Vector of Employees in that department
+    let hm: HashMap<String, Vec<String>> = HashMap::new();
+}
+
+// code user input and loop
+
+// add employee (string) to vector
+
+// print list of all people in a department, alphabetically
+
+// print all people in company, alphabetically
