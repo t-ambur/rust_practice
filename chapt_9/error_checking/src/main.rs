00978f577233ce8f8828953e6a17e3ec9b34f827
trevor <trevoramburgey@gmail.com>
2021-02-17 19:39:11 -0500

    chapt 9 examples

---
+++ chapt_9/error_checking/src/main.rs
@@ -0,0 +1,60 @@
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::io::ErrorKind;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+
+fn read_username_from_file4() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn main() {
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let f = File::open("hello.txt")?;
+
+//     Ok(())
+// }
